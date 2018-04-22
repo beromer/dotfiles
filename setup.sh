9ef58c7e1e91cfcb8ad5f6843c233027c9b3f196
Brian Romero <beromer@unm.edu>
2018-04-22 10:22:20 -0600

    added .tmp directory creation to setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 rm -f ~/.vimrc
 rm -f ~/.bash_common
 
+mkdir ~/.tmp
+
 ln -s "$(realpath .vimrc)" ~/.vimrc
 ln -s "$(realpath .bash_common)"  ~/.bash_common
 echo "source ~/.bash_common" >> ~/.bashrc
